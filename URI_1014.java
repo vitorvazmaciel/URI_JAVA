5772458cf45235cefd1f7014b482b7a0486d2e19
Vitor Vaz Maciel <53957535+vitorvazmaciel@users.noreply.github.com>
2019-11-26 10:18:01 -0400

    Update URI_1014.java

--- URI_1014.java
+++ URI_1014.java
@@ -1,13 +1,18 @@
+/**
+ *
+ * @author vitorvazmaciel
+ */
+
 public class URI_1014
 {
 	public static void main(String [] args)
 	{
-    java.util.Scanner tc = new java.util.Scanner(System.in);
+    		java.util.Scanner tc = new java.util.Scanner(System.in);
 
-    int distancia = tc.nextInt();
-	  double comb = tc.nextDouble();
+    	  	int distancia = tc.nextInt();
+	  	double comb = tc.nextDouble();
 	
-	  System.out.printf("%1.3f km/l\n", total(distancia, comb));
+	  	System.out.printf("%1.3f km/l\n", total(distancia, comb));
  	}
  	
  	static double total(int a, double b)
