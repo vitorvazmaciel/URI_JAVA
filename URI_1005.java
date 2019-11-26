f74399aecba616f55a667b30ed3299a02c3aa428
Vitor Vaz Maciel <53957535+vitorvazmaciel@users.noreply.github.com>
2019-11-25 22:08:46 -0400

    Update URI_1005.java

--- URI_1005.java
+++ URI_1005.java
@@ -2,9 +2,9 @@ public class URI_1005.java
 {
 	public static void main(String [] args)
 	{
-        java.util.Scanner tc = new java.util.Scanner(System.in);
+              java.util.Scanner tc = new java.util.Scanner(System.in);
 
-        double a = tc.nextDouble();
+              double a = tc.nextDouble();
 	      double b = tc.nextDouble();
 	
 	      System.out.printf("MEDIA = %1.5f\n", media(a, b));
