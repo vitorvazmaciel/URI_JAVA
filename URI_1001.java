1ff0c4c40b3adca2c8dc5c16d5522e4db25984a7
Unblemished <53957535+Unblemished@users.noreply.github.com>
2019-11-25 21:47:47 -0400

    Create URI_1001.java

---
+++ URI_1001.java
@@ -0,0 +1,17 @@
+public class URI_1001
+{
+    public static void main(String [] args)
+    {
+    java.util.Scanner tc = new java.util.Scanner(System.in);
+    
+    int a = tc.nextInt();
+    int b = tc.nextInt();
+
+    System.out.println("X = "+result(a, b));
+    }
+    
+    static int result(int a, int b)
+    {
+        return a+b;
+    }
+}
