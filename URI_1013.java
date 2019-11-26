115314087964ca002e6fbf49a05fed996c590068
Vitor Vaz Maciel <53957535+vitorvazmaciel@users.noreply.github.com>
2019-11-26 10:14:11 -0400

    Create URI_1013.java

---
+++ URI_1013.java
@@ -0,0 +1,31 @@
+/**
+ *
+ * @author vitorvazmaciel
+ */
+
+public class URI_1013
+{
+	public static void main(String [] args)
+	{
+    java.util.Scanner tc = new java.util.Scanner(System.in);
+
+	int a = tc.nextInt();
+	int b = tc.nextInt();
+	int c = tc.nextInt();
+	
+	int maior = a;
+	if((a<b) || (a<c))
+	{
+		if(b<c)
+		{
+		    maior = c;
+		}else
+		{
+		    maior = b;
+		}
+	}
+	
+    System.out.println(maior+" eh o maior");
+   
+ 	}
+}
