6dc51a73dd4d498c02034103f9592d43eda1dca4
Vitor Vaz Maciel <53957535+vitorvazmaciel@users.noreply.github.com>
2019-11-25 22:12:50 -0400

    Create URI_1007.java

---
+++ URI_1007.java
@@ -0,0 +1,19 @@
+public class Main
+{
+	public static void main(String [] args)
+	{
+      java.util.Scanner tc = new java.util.Scanner(System.in);
+
+      int a = tc.nextInt();
+	    int b = tc.nextInt();
+	    int c = tc.nextInt();
+	    int d = tc.nextInt();
+	    
+	    System.out.println("DIFERENCA = "+dif(a, b, c, d));
+ 	}
+ 	
+ 	static int dif(int a, int b, int c, int d)
+ 	{
+ 	    return (a*b - c*d);
+ 	}
+}
