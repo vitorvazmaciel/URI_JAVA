2421ed3de9dde1b27148e5b5f09d1d79e671495c
Vitor Vaz Maciel <53957535+vitorvazmaciel@users.noreply.github.com>
2019-11-25 22:07:30 -0400

    Update URI_1001.java

--- URI_1001.java
+++ URI_1001.java
@@ -2,12 +2,12 @@ public class URI_1001
 {
     public static void main(String [] args)
     {
-    java.util.Scanner tc = new java.util.Scanner(System.in);
+        java.util.Scanner tc = new java.util.Scanner(System.in);
     
-    int a = tc.nextInt();
-    int b = tc.nextInt();
+        int a = tc.nextInt();
+        int b = tc.nextInt();
 
-    System.out.println("X = "+result(a, b));
+        System.out.println("X = "+result(a, b));
     }
     
     static int result(int a, int b)
