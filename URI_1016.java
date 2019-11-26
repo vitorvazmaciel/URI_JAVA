a0486522c18f69deba23bc6d792359529935ab17
Vitor Vaz Maciel <53957535+vitorvazmaciel@users.noreply.github.com>
2019-11-26 10:33:57 -0400

    Update URI_1016.java

--- URI_1016.java
+++ URI_1016.java
@@ -7,10 +7,10 @@ public class URI_1016
 {
     public static void main(String [] args)
     {
-    java.util.Scanner tc = new java.util.Scanner(System.in);
-    int dis = tc.nextInt();
+        java.util.Scanner tc = new java.util.Scanner(System.in);
+        int dis = tc.nextInt();
     
-    System.out.println(mult(dis)+" minutos");
+        System.out.println(mult(dis)+" minutos");
     }
     
     static int mult(int a)
