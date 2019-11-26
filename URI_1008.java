3ce21e1fb6eb1213881dd01509a7903d2a36fa4a
Vitor Vaz Maciel <53957535+vitorvazmaciel@users.noreply.github.com>
2019-11-25 22:19:21 -0400

    Update URI_1008.java

--- URI_1008.java
+++ URI_1008.java
@@ -1,15 +1,15 @@
 public class URI_1008
 {
-	public static void main(String [] args)
-	{
+    public static void main(String [] args)
+    {
         java.util.Scanner tc = new java.util.Scanner(System.in);
 
         int func = tc.nextInt();
-	      double horas = tc.nextInt();
-	      double valor = tc.nextDouble();
+	double horas = tc.nextInt();
+	double valor = tc.nextDouble();
 	    
-	      System.out.println("NUMBER = " + func);
-	      System.out.printf("SALARY = U$ %1.2f\n", salario(horas, valor));
+	System.out.println("NUMBER = " + func);
+	System.out.printf("SALARY = U$ %1.2f\n", salario(horas, valor));
     }
     
     static double salario(double a, double b)
