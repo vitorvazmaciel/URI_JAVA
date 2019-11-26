c128805b4ba0f68f5b87fff5db03dacec7f4f712
Vitor Vaz Maciel <53957535+vitorvazmaciel@users.noreply.github.com>
2019-11-26 09:50:27 -0400

    Update URI_1011.java

--- URI_1011.java
+++ URI_1011.java
@@ -5,14 +5,14 @@
 
 public class URI_1011
 {
-	public static void main(String [] args){
-    java.util.Scanner tc = new java.util.Scanner(System.in);
+	public static void main(String [] args)
+	{
+    		java.util.Scanner tc = new java.util.Scanner(System.in);
 
-	double raio = tc.nextDouble();
+		double raio = tc.nextDouble();
 
-	System.out.printf("VOLUME = %1.3f\n", volume(raio));
-
- 	}
+		System.out.printf("VOLUME = %1.3f\n", volume(raio));
+	}
  	
  	static double volume(double r)
  	{
