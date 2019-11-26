23d8369e0751fdfa8c00bc07d62a06ea01fdc2e3
Vitor Vaz Maciel <53957535+vitorvazmaciel@users.noreply.github.com>
2019-11-26 09:49:53 -0400

    Create URI_1011.java

---
+++ URI_1011.java
@@ -0,0 +1,21 @@
+/**
+ *
+ * @author vitorvazmaciel
+ */
+
+public class URI_1011
+{
+	public static void main(String [] args){
+    java.util.Scanner tc = new java.util.Scanner(System.in);
+
+	double raio = tc.nextDouble();
+
+	System.out.printf("VOLUME = %1.3f\n", volume(raio));
+
+ 	}
+ 	
+ 	static double volume(double r)
+ 	{
+ 	    return 4.0/3 * 3.14159 * (r * r * r);
+ 	}
+}
