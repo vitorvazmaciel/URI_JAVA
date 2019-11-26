4a02855b88710ea7953da8ea3006c93f9b48849d
Vitor Vaz Maciel <53957535+vitorvazmaciel@users.noreply.github.com>
2019-11-26 10:33:34 -0400

    Create URI_1016.java

---
+++ URI_1016.java
@@ -0,0 +1,20 @@
+/**
+ *
+ * @author vitorvazmaciel
+ */
+
+public class URI_1016
+{
+    public static void main(String [] args)
+    {
+    java.util.Scanner tc = new java.util.Scanner(System.in);
+    int dis = tc.nextInt();
+    
+    System.out.println(mult(dis)+" minutos");
+    }
+    
+    static int mult(int a)
+    {
+        return a *= 2;
+    }
+}
