1cfc4a9c6493dc86a3cc45dc2d847cfa0d704b6a
Vitor Vaz Maciel <53957535+vitorvazmaciel@users.noreply.github.com>
2019-11-25 22:27:35 -0400

    Update URI_1009.java

--- URI_1009.java
+++ URI_1009.java
@@ -2,13 +2,13 @@ public class URI_1009
 {
 	public static void main(String [] args)
 	{
-    java.util.Scanner tc = new java.util.Scanner(System.in);
+    		java.util.Scanner tc = new java.util.Scanner(System.in);
 
-	String nome = tc.nextLine();
-    double salario = tc.nextDouble();
-	double vendas = tc.nextDouble();
+		String nome = tc.nextLine();
+    		double salario = tc.nextDouble();
+		double vendas = tc.nextDouble();
 	
-	System.out.printf("TOTAL = R$ %1.2f\n", total(salario ,comissao(vendas)));
+		System.out.printf("TOTAL = R$ %1.2f\n", total(salario ,comissao(vendas)));
 	}
 	
 	static double comissao(double a)
