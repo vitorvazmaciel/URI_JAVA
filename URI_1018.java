c6011f368daa0b47370cfefb93038e7e5ef20556
Vitor Vaz Maciel <53957535+vitorvazmaciel@users.noreply.github.com>
2019-11-26 10:43:41 -0400

    Update URI_1018.java

--- URI_1018.java
+++ URI_1018.java
@@ -7,9 +7,9 @@ public class URI_2018
 {
 	public static void main(String [] args)
 	{
-        java.util.Scanner tc = new java.util.Scanner(System.in);
+            java.util.Scanner tc = new java.util.Scanner(System.in);
 
-        double N = tc.nextDouble();
+            double N = tc.nextDouble();
    
 	    if((N>0) && (N<1000000))
 	    {
@@ -21,12 +21,12 @@ public class URI_2018
 	        System.out.println((int)N/20+" nota(s) de R$ 20,00");
 	            N=N%20;
 	        System.out.println((int)N/10+" nota(s) de R$ 10,00");
-                N=N%10;
+               	    N=N%10;
 	        System.out.println((int)N/5+" nota(s) de R$ 5,00");
 	            N=N%5;
 	        System.out.println((int)N/2+" nota(s) de R$ 2,00");
 	            N=N%2;
 	        System.out.println((int)N/1+" nota(s) de R$ 1,00");
 	    }
-   }
+   	}
 }
