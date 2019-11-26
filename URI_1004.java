c322387ca357dc470b5244713edc0f40d01cada7
Vitor Vaz Maciel <53957535+vitorvazmaciel@users.noreply.github.com>
2019-11-25 21:57:35 -0400

    Create URI_1004.java

---
+++ URI_1004.java
@@ -0,0 +1,17 @@
+public class URI_1004
+{
+	public static void main(String [] args)
+	{
+        java.util.Scanner tc = new java.util.Scanner(System.in);
+   
+        int a = tc.nextInt();
+        int b = tc.nextInt();
+
+        System.out.println("PROD = "+prod(a, b));
+    }
+    
+    static int prod(int a, int b)
+    {
+        return a*b;
+    }
+}
