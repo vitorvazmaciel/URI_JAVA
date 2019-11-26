2572795719004ce99309f43ee7d5aa4e0434df64
Vitor Vaz Maciel <53957535+vitorvazmaciel@users.noreply.github.com>
2019-11-25 22:26:14 -0400

    Create URI_1009.java

---
+++ URI_1009.java
@@ -0,0 +1,24 @@
+public class URI_1009
+{
+	public static void main(String [] args)
+	{
+    java.util.Scanner tc = new java.util.Scanner(System.in);
+
+	String nome = tc.nextLine();
+    double salario = tc.nextDouble();
+	double vendas = tc.nextDouble();
+	
+	System.out.printf("TOTAL = R$ %1.2f\n", total(salario ,comissao(vendas)));
+	}
+	
+	static double comissao(double a)
+	{
+	    return (15*a) / 100;
+	}
+	
+	static double total(double a, double b)
+	{
+	    return a+b;
+	}
+
+}
