b1eb74d98c303750fc01c277fbda5b44291a6574
Unblemished <53957535+Unblemished@users.noreply.github.com>
2019-11-25 21:48:26 -0400

    Create URI_1002.java

---
+++ URI_1002.java
@@ -0,0 +1,16 @@
+public class URI_1002
+{
+    public static void main(String [] args)
+    {
+    java.util.Scanner tc = new java.util.Scanner(System.in);
+   
+    double raio = tc.nextDouble();
+
+    System.out.printf("A=%2.4f\n",circ(raio));
+    }
+    
+    static double circ(double c)
+    {
+        return c = 3.14159 * (c*c);
+    }
+}
