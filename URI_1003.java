738b87d948a971b5e8078f8a9fdfa943a1e14c5c
Unblemished <53957535+Unblemished@users.noreply.github.com>
2019-11-25 21:51:38 -0400

    Create URI_1003.java

---
+++ URI_1003.java
@@ -0,0 +1,17 @@
+public class URI_1003
+{
+	public static void main(String [] args)
+	{
+        java.util.Scanner tc = new java.util.Scanner(System.in);
+   
+        int a = tc.nextInt();
+        int b = tc.nextInt();
+
+        System.out.println("SOMA = "+soma(a, b));
+    }
+    
+    static int soma(int a, int b)
+    {
+        return a+b;
+    }
+}
