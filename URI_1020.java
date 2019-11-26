0a2d6632ed38b31c26b9ee0ad945fa52a9cd3c7b
Vitor Vaz Maciel <53957535+vitorvazmaciel@users.noreply.github.com>
2019-11-26 10:48:37 -0400

    Create URI_1020.java

---
+++ URI_1020.java
@@ -0,0 +1,24 @@
+/**
+ *
+ * @author vitorvazmaciel
+ */
+
+public class URI_1020
+{
+  public static void main(String [] args)
+  {  
+    java.util.Scanner tc = new java.util.Scanner(System.in);
+  
+    int anostotal = tc.nextInt();
+    int anos, meses, dias;
+  
+    anos = (int)anostotal / 365;
+	  meses = (int)(anostotal % 365) / 30;
+    dias = (int)(anostotal % 365) % 30;
+
+    System.out.println(anos+" ano(s)");	
+    System.out.println(meses+" mes(es)");	
+    System.out.println(dias+" dia(s)");
+      
+  }
+}
