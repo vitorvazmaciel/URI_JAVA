c6f0ead06f4bcece927a451f43f1ac285292950e
Vitor Vaz Maciel <53957535+vitorvazmaciel@users.noreply.github.com>
2019-11-26 10:29:45 -0400

    Create URI_1015.java

---
+++ URI_1015.java
@@ -0,0 +1,32 @@
+/**
+ *
+ * @author vitorvazmaciel
+ */
+
+public class Main
+{
+        public static void main(String [] args)
+        {
+          java.util.Scanner tc = new java.util.Scanner(System.in);
+
+	        Main a = new Main();
+          Main b = new Main();
+        
+          a.x = tc.nextDouble();
+          a.y = tc.nextDouble();
+        
+          b.x = tc.nextDouble();
+          b.y = tc.nextDouble();
+	
+          System.out.printf("%.4f\n", Math.sqrt(oper(a.x, b.x, a.y, b.y)));
+        }
+        
+    private double x;
+    private double y;
+
+    static double oper(double a, double b, double c, double d)
+    {
+        return ((b - a)*(b - a)) + ((d - c)*(d - c));
+    }
+        
+}
