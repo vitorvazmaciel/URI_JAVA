ccdcf02258f8dff6471d2a19ce9bb57e43e13640
Vitor Vaz Maciel <vittorvaz13@gmail.com>
2019-12-01 22:25:37 -0300

    Create URI_1050.java

---
+++ URI_1050.java
@@ -0,0 +1,53 @@
+/**
+ *
+ * @author vitorvazmaciel
+ */
+
+public class URI_1050
+{
+	public static void main(String [] args)
+	{
+		java.util.Scanner tc = new java.util.Scanner(System.in);
+
+		int n = tc.nextInt();
+
+		switch(n)
+		{
+			case 61:
+				System.out.println("Brasilia");
+			break;
+
+			case 71:
+				System.out.println("Salvador");
+			break;
+
+			case 11:
+				System.out.println("Sao Paulo");
+			break;
+
+			case 21:
+				System.out.println("Rio de Janeiro");
+			break;
+
+			case 32:
+				System.out.println("Juiz de Fora");
+			break;
+
+			case 19:
+				System.out.println("Campinas");
+			break;
+
+			case 27:
+				System.out.println("Vitoria");
+			break;
+
+			case 31:
+				System.out.println("Belo Horizonte");
+			break;
+
+			default:
+				System.out.println("DDD nao cadastrado");
+			break;
+		}
+	}	
+}
